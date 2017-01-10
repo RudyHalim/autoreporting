a33d26cf5438e14b2f3053156f14d3fa13dde20d
Rudy Halim <rudyhalim.microsoft@gmail.com>
2017-01-10 15:00:21 +0700

    commit v1.0

--- assets/grephit.php
+++ assets/grephit.php
@@ -4,7 +4,12 @@ $date = isset($_GET['d']) && !empty($_GET['d']) ? str_replace("-", ".", $_GET['d
 
 if($menu != '' && $date != '') {
 
-	$hit = shell_exec("grep menu=$menu /data/apache/apache2_80/logs/access_log.$date | wc -l");
-	echo $hit;
+	if($menu == 'regmeme22m') {
+		$menu = 'menu=regmeme22m';
+	}
+
+	$shell_command = "grep $menu /data/apache/apache2_80/logs/access_log.$date | wc -l";
+	$hit = shell_exec($shell_command);
+	echo number_format($hit);
 
 }
